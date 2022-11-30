96447d5cdef0aa4934c2a2a896a29e81d4e62729
PhamTung <phamtritungtdtt@gmail.com>
2022-12-01 00:28:10 +0900

    fixed

--- javascripts/demo.js
+++ javascripts/demo.js
@@ -16,11 +16,8 @@ if (biicore.bgMusic) {
 			audioPlayer.setAttribute("src", "./MotNha.mp3");
 			document.getElementsByClassName("bii-player")[0].style.display = "block";
 		}
-
-		audioPlayer.volume = 0.8;
+		audioPlayer.volume = 0.3;
 		audioPlayer.setAttribute("controls", "controls");
-		audioPlayer.setAttribute("autoplay", "");
-		audioPlayer.setAttribute("loop", "");
 		document.body.appendChild(audioPlayer);
 	}, 1000);
 
@@ -36,10 +33,8 @@ if (biicore.bgMusic) {
 		}
 	}, 200);
 
-
 	function playPause() {
-		document.getElementsByClassName("bii-player")[0].classList.add("show-sec");
-
+		document.getElementsByClassName("bii-player")[0].classList.remove("show-sec");
 		if (audioPlayer.paused) {
 			audioPlayer.play();
 			document.getElementById("playerVolumeOff").style.display = "none";
@@ -94,14 +89,14 @@ if (biicore.bgMusic) {
 	}
 	</style>
 	<div class="bii-player">
-		<div onclick="playPause();" class="bii-player-secondary"><div class="bii-player-secondary-content">Click vào để tắt nhạc!</div></div>
+		<div onclick="playPause();" class="bii-player-secondary"><div class="bii-player-secondary-content">Click vào để phát nhạc!</div></div>
 		<div onclick="playPause();" class="playerIcon">
-			<span style="display:none;"  id="playerVolumeOff">
+			<span id="playerVolumeOff">
 				<svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="#fff" class="bi bi-volume-mute-fill" viewBox="0 0 16 16">
 				  <path d="M6.717 3.55A.5.5 0 0 1 7 4v8a.5.5 0 0 1-.812.39L3.825 10.5H1.5A.5.5 0 0 1 1 10V6a.5.5 0 0 1 .5-.5h2.325l2.363-1.89a.5.5 0 0 1 .529-.06zm7.137 2.096a.5.5 0 0 1 0 .708L12.207 8l1.647 1.646a.5.5 0 0 1-.708.708L11.5 8.707l-1.646 1.647a.5.5 0 0 1-.708-.708L10.793 8 9.146 6.354a.5.5 0 1 1 .708-.708L11.5 7.293l1.646-1.647a.5.5 0 0 1 .708 0z"/>
 				</svg>
 			</span>
-			<span id="playerVolumeOn">
+			<span style="display:none;" id="playerVolumeOn">
 				<svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="#fff" class="bi bi-volume-up-fill" viewBox="0 0 16 16">
 				  <path d="M11.536 14.01A8.473 8.473 0 0 0 14.026 8a8.473 8.473 0 0 0-2.49-6.01l-.708.707A7.476 7.476 0 0 1 13.025 8c0 2.071-.84 3.946-2.197 5.303l.708.707z"/>
 				  <path d="M10.121 12.596A6.48 6.48 0 0 0 12.025 8a6.48 6.48 0 0 0-1.904-4.596l-.707.707A5.483 5.483 0 0 1 11.025 8a5.483 5.483 0 0 1-1.61 3.89l.706.706z"/>
